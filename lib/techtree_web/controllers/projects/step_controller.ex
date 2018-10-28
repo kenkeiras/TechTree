bff33df5d1a3a2afb7ff886a2eaf38c0d9bfd3bf
kenkeiras <kenkeiras@codigoparallevar.com>
2018-10-29 00:24:05 +0100

    Remove views no longer used.

--- lib/techtree_web/controllers/projects/step_controller.ex
+++ lib/techtree_web/controllers/projects/step_controller.ex
@@ -8,14 +8,7 @@ defmodule TechtreeWeb.Projects.StepController do
   alias TechtreeWeb.Projects.Plugs
 
   plug :require_existing_contributor
-
-  plug :authorize_page_visibility when action in [:show]
-  plug :authorize_page_edition when action in [
-    :new, :create,
-    :edit, :mark_completed, :mark_uncompleted,
-    :update, :delete,
-    :api_remove, :api_patch, :api_create
-  ]
+  plug :authorize_page_edition
 
   defp require_existing_contributor(conn, x) do
     # TODO move to it's own module
@@ -30,11 +23,6 @@ defmodule TechtreeWeb.Projects.StepController do
     ProjectController.authorize_page_edition(conn, x)
   end
 
-  def new(conn, _params) do
-    changeset = Projects.change_step(%Step{})
-    render(conn, "new.html", changeset: changeset)
-  end
-
   def create(conn, %{"step" => step_params}) do
     case Projects.create_step(conn.assigns.current_contributor, conn.assigns.project, step_params) do
       {:ok, step} ->
@@ -44,17 +32,6 @@ defmodule TechtreeWeb.Projects.StepController do
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
-  end
-
-  def show(conn, %{"step_id" => id}) do
-    step = Projects.get_step_with_dependencies!(id)
-    render(conn, "show.html", step: step)
-  end
-
-  def edit(conn, %{"step_id" => id}) do
-    step = Projects.get_step!(id)
-    changeset = Projects.change_step(step)
-    render(conn, "edit.html", step: step, changeset: changeset)
   end
 
   ## TODO : Refactor marking as completed/uncomplete
@@ -68,8 +45,8 @@ defmodule TechtreeWeb.Projects.StepController do
 
       {:error, %Ecto.Changeset{} = changeset} ->
           render(conn, "edit.html", step: step, changeset: changeset)
-      end
     end
+  end
 
   def mark_uncompleted(conn, %{"step_id" => id}) do
     step = Projects.get_step!(id)
@@ -81,29 +58,7 @@ defmodule TechtreeWeb.Projects.StepController do
 
       {:error, %Ecto.Changeset{} = changeset} ->
           render(conn, "edit.html", step: step, changeset: changeset)
-      end
     end
-
-    def update(conn, %{"step_id" => id, "step" => step_params}) do
-    step = Projects.get_step!(id)
-
-    case Projects.update_step(step, step_params) do
-      {:ok, step} ->
-        conn
-        |> put_flash(:info, "Step updated successfully.")
-        |> redirect(to: project_step_path(conn, :show, conn.assigns.project.id, step))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", step: step, changeset: changeset)
-    end
-  end
-
-  def delete(conn, %{"step_id" => id}) do
-    step = Projects.get_step!(id)
-    {:ok, _step} = Projects.delete_step(step)
-
-    conn
-    |> put_flash(:info, "Step deleted successfully.")
-    |> redirect(to: project_project_path(conn, :show, conn.assigns.project.id))
   end
 
   def api_remove(conn, %{"step_id" => id}) do
