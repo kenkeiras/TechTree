8398702a9842574fe5196f4c68e061d468c09395
kenkeiras <kenkeiras@codigoparallevar.com>
2018-10-28 21:09:14 +0100

    Add step state field + migration with translation.
    States are:
     * Todo (previously represented by non-completed steps).
     * Completed
     * Archived (new)
     * Work-in-progress (new)

--- lib/techtree/projects/step.ex
+++ lib/techtree/projects/step.ex
@@ -2,12 +2,13 @@ defmodule Techtree.Projects.Step do
   use Ecto.Schema
   import Ecto.Changeset
 
-  alias Techtree.Projects.{Dependency, Project, Step}
+  alias Techtree.Projects.{Dependency, Project, Step, StepState}
 
   schema "steps" do
     field :description, :string
     field :title, :string
     field :completed, :boolean
+    field :state, StepState
     belongs_to :project, Project
     many_to_many :dependencies, Step, join_through: "dependencies",
                                       join_keys: [depender_id: :id, depended_id: :id]
@@ -18,7 +19,7 @@ defmodule Techtree.Projects.Step do
   @doc false
   def changeset(step, attrs) do
     step
-    |> cast(attrs, [:title, :description, :completed])
+    |> cast(attrs, [:title, :description, :completed, :state])
     |> validate_required([:title])
   end
 end
