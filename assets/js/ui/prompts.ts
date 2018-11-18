34332cb98225a848546fbe53dd32d4e3adfb461e
kenkeiras <kenkeiras@codigoparallevar.com>
2018-11-18 20:38:26 +0100

    Add title option to dangerous action confirmation dialog.

--- assets/js/ui/prompts.ts
+++ assets/js/ui/prompts.ts
@@ -2,6 +2,7 @@ import * as Api from '../api';
 import * as Hotkeys from '../hotkeys';
 
 const DEFAULT_DANGER = 'This cannot be undone!';
+const DEFAULT_DANGER_TITLE = 'Confirm action';
 
 export function show_add_step_prompt(project_id: string, steps) {
     build_popup((popup) => add_step_prompt(popup, project_id, steps));
@@ -215,10 +216,20 @@ export function build_popup(setup_prompt: Function){
     return popup;
 }
 
-export function confirm_dangerous_action(action_description, type_information, callback, options?) {
+export interface DangerousActionOptions {
+    danger: string, // What will happen?
+    title: string, // Title for the confirmation popup
+}
+
+export function confirm_dangerous_action(action_description, type_information, callback, options?: DangerousActionOptions) {
     let danger = DEFAULT_DANGER;
     if ((options !== undefined) && (options.danger !== undefined)) {
         danger = options.danger;
+    }
+
+    let titleMsg = DEFAULT_DANGER_TITLE;
+    if ((options !== undefined) && (options.title !== undefined)) {
+        titleMsg = options.title;
     }
 
     const overlay = document.createElement("div");
@@ -233,6 +244,10 @@ export function confirm_dangerous_action(action_description, type_information, c
 
     overlay.style.height = dimensions.height + 'px';
     overlay.style.width = dimensions.width + 'px';
+
+    const title = document.createElement('h1');
+    title.innerText = titleMsg;
+    popup.appendChild(title);
 
     const messageBox = document.createElement('div');
     messageBox.setAttribute('class', 'message');
